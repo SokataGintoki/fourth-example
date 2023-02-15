d984c09b31f0c4d07a7e91e553aa10c28249908f
Benizakura <catarsislogos@gmail.com>
2023-02-15 17:14:00 +0300

    Оператор опциональной цепочки

---
+++ second example/OperatorOpcionalMerger/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
+
+console.log(block);
+
+// if (block) {
+//     console.log(block.textContent);
+// };
+
+console.log(block?.textContent);
+
+
+console.log(1 + 2);
